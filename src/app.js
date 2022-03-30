dd6585bdb1a79f0cc2f0d1674b37fd909de2cc7b
Zumaiba - iProgrammer <zubi.z.n.25@gmail.com>
2022-03-30 18:25:05 +0530

    Setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-// const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
@@ -19,7 +18,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res)=>{
     res.render('index', {
-        title: 'Weather_App',
+        title: 'Weather-App',
         username: 'Zubi'
     })
 })
@@ -43,48 +42,7 @@ app.get('/weather', (req, res) => {
                 address: req.query.address
             })
     })
-
-    // geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-    //     if (error) {
-    //         return res.send({ error })
-    //     }
-
-    //     forecast(latitude, longitude, (error, forecastData) => {
-    //         if (error) {
-    //             return res.send({ error })
-    //         }
-
-    //         res.send({
-    //             forecast: forecastData,
-    //             location,
-    //             address: req.query.address
-    //         })
-    //     })
-    // })
 })
-
-// app.get('/weather', (req,res) => {
-//     const address = req.query.address
-
-//     if(!address){
-//         return res.send({
-//             error: "Please enter a location to search weather"
-//         })
-//     }
-//    forecast(address,(error, {temperature, description,cityName,humidity}) => {
-//        if(error){
-//            return res.send({
-//                error
-//            })
-//        } 
-//        console.log(temperature,description,cityName,humidity)
-//        res.send({
-//             cityName,
-//             temperature,
-//             cityName,
-//        })
-//    })
-// })
 
 
 app.get('*', (req, res)=>{
